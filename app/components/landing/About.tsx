b2af819c024e292e3d137ea5ccb39a85b1004bd6
soham <sohambasak42@gmail.com>
2024-07-23 02:16:31 +0530

    refactor: banner_image

--- app/components/landing/About.tsx
+++ app/components/landing/About.tsx
@@ -2,7 +2,7 @@ import Heading from '../ui/Heading';
 
 const About = () => {
   return (
-    <section className='prose max-w-[50%] text-center dark:prose-invert'>
+    <section className='prose max-w-3xl text-center dark:prose-invert'>
       <Heading
         classNames={{
           h1: 'text-center mx-auto text-2xl font-bold roboto-mono-500',
@@ -11,9 +11,10 @@ const About = () => {
         About Us
       </Heading>
 
-      <p className='roboto-mono-500 mt-16'>
-        Focus On Your Vision And Leave The Development Complexities To Us. EvolveDev's Streamlined
-        Approach Ensures Efficient Project Execution, Clear Communication, And Timely Delivery.
+      <p className='roboto-mono-500 mt-16 leading-loose'>
+        Focus On Your Vision And Leave The Development Complexities To Us. EvolveDev&apos;s
+        Streamlined Approach Ensures Efficient Project Execution, Clear Communication, And Timely
+        Delivery.
       </p>
     </section>
   );
