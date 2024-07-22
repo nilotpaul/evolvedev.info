c379ba02607c78cc8e742a6687ff3940c6ea460a
soham <sohambasak42@gmail.com>
2024-07-23 01:02:58 +0530

    style: homepage_fonts

--- app/components/landing/About.tsx
+++ app/components/landing/About.tsx
@@ -5,13 +5,13 @@ const About = () => {
     <section className='prose max-w-[50%] text-center dark:prose-invert'>
       <Heading
         classNames={{
-          h1: 'text-center mx-auto text-2xl font-bold',
+          h1: 'text-center mx-auto text-2xl font-bold roboto-mono-500',
         }}
       >
         About Us
       </Heading>
 
-      <p className='mt-16'>
+      <p className='roboto-mono-500 mt-16'>
         Focus On Your Vision And Leave The Development Complexities To Us. EvolveDev's Streamlined
         Approach Ensures Efficient Project Execution, Clear Communication, And Timely Delivery.
       </p>
