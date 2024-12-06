9f6f6c0ac5e2031675425389d792a9cab59cddb9
nilotpaul <nilotpaul.nandi2021@uem.edu.in>
2024-12-06 15:50:19 +0530

    style: re-ordered the hero section to left side

--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -1,11 +1,11 @@
 const Hero = () => {
   return (
-    <div className='mb-20 mt-8 space-y-14 text-center lg:text-start'>
+    <div className='mb-20 mt-8 space-y-14 text-start'>
       <h1 className='font-sans text-4xl font-bold leading-tight md:text-5xl md:leading-tight'>
         Empowering Developers to Evolve and Excel
       </h1>
 
-      <p className='text-lg font-medium leading-relaxed text-gray-600 dark:text-gray-400 lg:mx-0 lg:max-w-lg lg:text-xl'>
+      <p className='max-w-lg text-start text-lg font-medium leading-relaxed text-gray-600 dark:text-gray-400 lg:mx-0 lg:text-xl'>
         Powering the future of mordern development with EvolveDev.
       </p>
     </div>
